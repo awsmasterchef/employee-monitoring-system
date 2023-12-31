e7559f57b884629f6b569951d5ea3c37889db387
Abhishek <abc@gmail.com>
2023-12-31 14:47:11 +0530

    push changes

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ iot_endpoint= "a5ol8bk0t9cc0-ats.iot.ap-south-1.amazonaws.com"
 configuration_topic = "iot/test"
 
 
-aws_access_key_id = 'AKIAV6MW6JIS46JHYFP2'
-aws_secret_access_key = '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'
+aws_access_key_id = '******************'
+aws_secret_access_key = '****************'
 
 region_name = 'ap-south-1'
 config_bucket_name = 'employee-monitoring-store-config-store'
